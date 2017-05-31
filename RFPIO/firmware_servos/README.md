fdec783d3a0aea90318cd7bd9b266171c5263323
wassfila <wassim.filali@gmail.com>
2017-05-31 21:54:38 +0200

    new project specific for servos

---
+++ RFPIO/firmware_servos/README.md
@@ -0,0 +1,13 @@
+# First load the libraries
+`cmd>mbed deploy`
+
+# Compile
+`cmd>compile.bat`
+
+# Flash
+* install st-tools
+* run
+
+`cmd>flash.bat`
+
+
---
+++ RFPIO/firmware_servos/main.cpp
@@ -0,0 +1,74 @@
+#include "mbed.h"
+
+#include "rfmesh.h"
+#include "suart.h"
+
+#include "rfpio.h"
+
+Serial   rasp(PB_10, PB_11, 115200);
+//DigitalOut myled(PC_13);
+Ticker tick_call;
+//nRF Modules 1:Gnd, 2:3.3v, 3:ce,  4:csn, 5:sck, 6:mosi, 7:miso, 8:irq 
+//RFPIO Layout !!!!
+RfMesh mesh(&rasp,           PA_5,  PB_12, PB_13, PB_15, PB_14, PA_4);
+
+rfpio myBoard(&rasp);
+
+void the_ticker()
+{
+    //myled = !myled;
+    
+}
+
+void rf_message_received(uint8_t *data,uint8_t size)
+{
+    uint8_t length = data[0];
+    rasp.printf("rf>Rx message Handler : 0x");
+    for(int i = 0; i < length; i++)
+    {
+        rasp.printf(" %02x",data[i]);
+    }
+    rasp.printf("\r\n");
+    if(data[1] == 'P')
+    {
+        int val = data[4];
+        val <<= 8;
+        val |= data[3];
+        val <<= 8;
+        val |= data[2];
+        rasp.printf("PIO : 0x%08x\n",val);
+        myBoard.write(val);
+    }
+}
+
+void init()
+{
+    wait_ms(100);
+    rasp.printf("sizeof(int) : %d\n",sizeof(int));
+
+    rasp.printf("Hello RFPIO Board\n");
+
+    tick_call.attach(&the_ticker,1);
+
+    mesh.init();//left to the user for more flexibility on memory management
+
+    rasp.printf("selectChannel(22)\n");
+    mesh.nrf.selectChannel(22);
+
+    mesh.attach(&rf_message_received,RfMesh::CallbackType::Message);
+
+    myBoard.write(0x00FFFFFF);
+    wait(1.0);
+    myBoard.write(0x00000000);
+    wait(1.0);
+}
+
+int main() 
+{
+    init();
+
+    while(1) 
+    {
+        wait(1.0);
+    }
+}
